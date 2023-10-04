d1df7013e042686109f2ac729532483e3ca720eb
irobbins2 <isaiahdr12@gmail.com>
2023-10-04 09:31:53 -0500

    VariableWarmup

---
+++ VariableWarmup/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String personName = "John Doe";
+        int personAge = 25;
+
+        double productPrice = 19.99;
+
+        float tempatureCelcius = 27.5f;
+
+        boolean isLoggedIn = true;
+
+    }
+}
