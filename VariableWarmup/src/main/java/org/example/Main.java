e0f89b58add32c5f44569627ed9a9481e25ced00
irobbins2 <isaiahdr12@gmail.com>
2023-10-04 09:55:23 -0500

    updated to run variables

--- VariableWarmup/src/main/java/org/example/Main.java
+++ VariableWarmup/src/main/java/org/example/Main.java
@@ -2,16 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         String personName = "John Doe";
+        System.out.println(personName);
+
         int personAge = 25;
+        System.out.println(personAge);
 
         double productPrice = 19.99;
+        System.out.println(productPrice);
 
-        float tempatureCelcius = 27.5f;
+        float temperatureCelsius = 27.5f;
+        System.out.println(temperatureCelsius);
 
         boolean isLoggedIn = true;
+        System.out.println(isLoggedIn);
 
     }
 }
